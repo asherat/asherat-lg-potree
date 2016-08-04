4cb5d30443698f3296627cb29733bfa07836b05f
asherat <asherat666@gmail.com>
2016-08-04 15:22:24 +0200

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -7,67 +7,83 @@ log_file="mylog.log";
 cat /dev/null > $log_file;
 ##################################
 
-if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+if [[ $(/usr/bin/id -u) -ne 0 ]] ; then
     echo "Not running as root";
     exit;
 fi
 
+separator="----------------------------------------";
 usage="USAGE:$0 [UserInput(true,false)] ([LG_IP] [LG_USER] [LG_PASSWORD] IGNORED if UserInput = true)"
 
-if [ -z $1 || ( [ $1 !=  "true" ] && ( [ $1 != "false" ] && [ $# -ne 4 ] ) ) ; then
+if [[ ! ( $1 && ( $1 == "true" || ( $1 == "false" && $# -eq 4 ) ) ) ]] ; then
   echo $usage;
   exit 2;
 fi
 
 user_input=$1;
-#user_input=false;
 
+echo $separator;
 #Install all necessary packages
-echo "Installing packages";
-sudo apt-get -y install nodejs ssh php5 apache2 libssh2-php gcc-multilib sshpass >> $log_file;
+echo -ne "Installing packages ... ";
+sudo apt-get -y install nodejs ssh php5 apache2 libssh2-php gcc-multilib sshpass >> $log_file 2>&1 && \
+echo "COMPLETED";
 
 #Restart apache to load libssh2-php
-echo "Restarting apache";
-sudo service apache2 restart & >> $log_file;
-
+echo -ne "\nRestarting apache ... ";
+sudo service apache2 restart >> $log_file 2>&1 && \
+echo "COMPLETED";
+exit
 #Compile the spacenav-emitter driver
-echo "Compiling controller driver";
-gcc -m32 -o nodeServer/spacenav-emitter nodeServerh/spacenav-emitter.c >> $log_file;
+echo -ne "\nCompiling controller driver ... ";
+gcc -m32 -o nodeServer/spacenav-emitter nodeServerh/spacenav-emitter.c >> $log_file 2>&1 && \
+echo "COMPLETED";
 
 #Moving php-interface to apache
-echo "Copying php-interface";
+echo -ne "\nCopying php-interface ... ";
 web_path="/var/www/";
 if [ -d "/var/www/html" ] ; then
   web_path=$webpath"html/";
 fi
-sudo cp php-interface/* $web_path >> $log_file;
+sudo cp php-interface/* $web_path >> $log_file 2>&1 && \
+echo "COMPLETED";
 
 #Moving lg-potree files to the node server
-echo "Copying Potree Files";
-yes | cp -rf * $nodePath >> $log_file;
+echo -ne "\nCopying Potree Files ... ";
+yes | cp -rf * $nodePath >> $log_file 2>&1 && \
+echo "COMPLETED";
 
 #Create new ssh keys and make a duplicate for www-data
-echo "Creating ssh keys";
-ssh-keygen -t rsa -f $home/.ssh/id_rsa -q -P "" >> $log_file;
-echo "Moving keys and giving access to web user";
-sudo mkdir /opt/www-files >> $log_file;
-sudo chown www-data:www-data /opt/www-files >> $log_file;
-sudo chown www-data:www-data /opt/www-files/* >> $log_file;
+if [[ ! -f $HOME/.ssh/*id_rsa && ! -f $HOME/.ssh/*id_rsa.pub ]] ; then
+	echo -ne "\nUsing already existing SSH Keys";
+else
+	echo -ne "\nCreating ssh keys ... ";
+	ssh-keygen -t rsa -f $HOME/.ssh/id_rsa -q -P "" >> $log_file 2>&1 && \
+	echo "COMPLETED";
+fi
+echo -ne "\nMoving keys and giving access to web user ... ";
+sudo mkdir /opt/www-files >> $log_file 2>&1 \
+sudo cp $HOME/.ssh/*id_rsa* /opt/www-files >> $log_file 2>&1 && \
+sudo chown www-data:www-data /opt/www-files >> $log_file 2>&1 && \
+sudo chown www-data:www-data /opt/www-files/* >> $log_file 2>&1 && \
+echo "COMPLETED";
 
 #Copy the ssh keys to the Liquid Galaxy
-echo "Adquiring access to Liquid Galaxy";
-if [ $user_input ] ; then
+if [ $user_input == "true" ] ; then
+  echo $separator;
   echo "Type the IP of the remote Liquid Galaxy, followed by [ENTER]:"
-  read $lg_IP
+  read lg_IP
   echo "Type the username of the Liquid Galaxy, followed by [ENTER]:"
-  read $lg_user
+  read lg_user
   echo "Type the password of the Liquid Galaxy, followed by [ENTER]:"
-  read -s $lg_password
+  read -s lg_password
 else
-  $lg_IP=$2;
-  $lg_user=$3;
-  $lg_password=$4;
+  lg_IP=$2;
+  lg_user=$3;
+  lg_password=$4;
 fi
-sshpass -p $lg_password ssh-copy-id $lg_user"@"$lg_IP >> $log_file;
+echo -ne "\nAdquiring access to Liquid Galaxy at $lg_IP:$lg_user ... ";
+sshpass -p $lg_password ssh-copy-id $lg_user"@"$lg_IP >> $log_file 2>&1 && \
+echo "COMPLETED";
 
+echo $separator;
 echo "Completed Installation, you can check the log file at $log_file";
