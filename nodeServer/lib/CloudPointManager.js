b9442c444545ca9b2c62d396d9e59d81abb4a549
Ismael <asherat666@gmail.com>
2016-08-17 16:23:03 +0200

    Update CloudPointManager.js

--- nodeServer/lib/CloudPointManager.js
+++ nodeServer/lib/CloudPointManager.js
@@ -56,12 +56,6 @@ function CPMRelay( io, cpfile) {
 		socket.broadcast.emit('updateArgs', data);
 	});
 
-	//The manager sends new Args
-	socket.on('addData', function(data){
-		console.log("HOLA", data);
-	});
-	
-	
     });
 
   return {
